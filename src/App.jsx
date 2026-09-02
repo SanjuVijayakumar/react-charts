9a5e0db9d0ba337e6f87877ee7b81d2ff14be7ad
sanju <sanjukalathil05@gmail.com>
2026-09-03 01:53:50 +0530

    api data update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Chart from './components/Chart'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({
+    labels: [], 
+    datasets: []
+  })
+
+  useEffect(() => {
+    getChartData()
+  },[])
+
+  function getChartData() {
+    setData({
+      labels: ['Palakkad', 'Thrissur', 'Eranakulam', 'Trivandrum'],
+      datasets: [
+        {
+          label: 'Population',
+          data: [6756785, 4549611, 2156694, 5985997],
+          borderWidth: 1,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)'
+          ]
+        }
+      ]
+    })
+  }
 
   return (
     <>
-      <Chart />
+      <h1>Kerala Population</h1>
+      <Chart data={data} />
     </>
   )
 }
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import { Bar } from 'react-chartjs-2'
 import {
   Chart as ChartJS,
@@ -19,42 +18,26 @@ ChartJS.register(
   Legend
 )
 
-const Chart = (displayTitle = true, displayLegend = true, legendPosition = 'bottom') => {
-    const [data, setData] = useState({
-        labels: ['Palakkad', 'Thrissur', 'Eranakulam', 'Trivandrum'],
-        datasets: [
-            {
-            label: 'Population',
-            data: [6756785, 4549611, 2156694, 5985997],
-            borderWidth: 1,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(54, 162, 235, 0.6)',
-                'rgba(255, 206, 86, 0.6)',
-                'rgba(75, 192, 192, 0.6)' 
-            ]
-            }
-        ]
-    })
-    
+const Chart = ({ data }) => {
+
   return (
     <div className="chart">
-      <Bar 
+      <Bar
         data={data}
         options={{
-            plugins: {
-                title: {
-                    display: displayTitle,
-                    text: 'Largest Cities in Kerala',
-                    font: {
-                        size: 25
-                    }
-                },
-                legend: {
-                    display: displayLegend,
-                    position: legendPosition
-                }
+          plugins: {
+            title: {
+              display: true,
+              text: 'Largest Cities in Kerala',
+              font: {
+                size: 25
+              }
+            },
+            legend: {
+              display: true,
+              position: 'bottom'
             }
+          }
         }}
       />
     </div>
